1ed276d2b40af8a9ac7b25a27272812dd8d18619
Oscar Linderholm <osm@recv.se>
2019-05-04 17:37:59 +0200

    Update tags

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -27,8 +27,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Google-chrome",	NULL, 		NULL, 	1 << 1, 	0, 		-1 },
+	{ "Slack",		NULL,		NULL,	1 << 2,		0,		-1 },
+	{ "Keepassx",		NULL,		NULL,	1 << 3,		0,		-1 },
 };
 
 /* layout(s) */
